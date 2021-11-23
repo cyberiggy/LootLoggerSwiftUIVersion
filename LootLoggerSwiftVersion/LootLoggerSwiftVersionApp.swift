0e375cf792934c31dfb57eee054b174f4332ac63
Ron Woodbury <ronwoodbury@mac.com>
2021-11-22 20:30:06 -0700

    Initial Commit

---
+++ LootLoggerSwiftVersion/LootLoggerSwiftVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootLoggerSwiftVersionApp.swift
+//  LootLoggerSwiftVersion
+//
+//  Created by Ron Woodbury on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LootLoggerSwiftVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
